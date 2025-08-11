c3a6a18fa3316e73c8e9996f07de402c21a32767
Krrish-Devani <krrishdevani84@gmail.com>
2025-08-11 10:45:39 +0530

    Express and Mongodb setup

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+import connectDB from './lib/mongoDB.js';
+
+dotenv.config();
+
+const app = express(); 
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json({ limit: '10mb' }));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = 'Something went wrong' } = err;
+    res.status(statusCode).json({message});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on the port http://localhost:${PORT}`);
+    connectDB();
+})
